e327a582f11aa3f9d11001bd1b2798fdabd28695
sc-mitton <scm.mitton@gmail.com>
2025-06-18 10:06:35 -0700

    fix match_results table

--- supabase/migrations/20250617175646_match_results.sql
+++ supabase/migrations/20250617175646_match_results.sql
@@ -1,6 +1,7 @@
 create table public.match_results (
-  id uuid not null references auth.users on delete cascade,
-  winner uuid not null references auth.users on delete cascade,
+  id uuid not null default uuid_generate_v4(),
+  winner uuid not null references public.profiles(id) on delete cascade,
+  loser uuid not null references public.profiles(id) on delete cascade,
   match_id bigint not null references public.matches on delete cascade,
   winner_score integer not null,
   loser_score integer not null,
