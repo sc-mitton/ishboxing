d81151c44017432abd3e42b9a02412c969e03789
sc-mitton <scm.mitton@gmail.com>
2025-06-18 08:35:01 -0700

    fix match results migration

--- supabase/migrations/20250617175646_match_results.sql
+++ supabase/migrations/20250617175646_match_results.sql
@@ -1,7 +1,7 @@
 create table public.match_results (
   id uuid not null references auth.users on delete cascade,
   winner uuid not null references auth.users on delete cascade,
-  match_id uuid not null references public.matches on delete cascade,
+  match_id bigint not null references public.matches on delete cascade,
   winner_score integer not null,
   loser_score integer not null,
   created_at timestamp with time zone not null default now(),
